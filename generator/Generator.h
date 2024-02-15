643bdc0f101548f9d9c25b0b878cdb6127f0c62a
JeongminSung <hellojeongmin@gmail.com>
2024-02-15 14:03:13 +0900

    generator

---
+++ generator/Generator.h
@@ -0,0 +1,16 @@
+#include <map>
+#include <list>
+#include <tuple>
+#include <vector>
+#include <iomanip>
+#include "Node.h"
+#include "Code.h"
+
+using std::max;
+using std::cout;
+using std::setw;
+using std::endl;
+using std::map;
+using std::list;
+using std::tuple;
+using std::vector;
